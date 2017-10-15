9a6808b4a9569a425c2f6ae80a2cd2d21bda5d62
X_Smart <xchaigl@gmail.com>
2017-10-15 15:33:14 +0800

    Update Demo1.java

--- src/com/itheima/test/Demo1.java
+++ src/com/itheima/test/Demo1.java
@@ -5,6 +5,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello GitHub");
+		System.out.println("你好！！GitHub！");
 	}
 
 }
