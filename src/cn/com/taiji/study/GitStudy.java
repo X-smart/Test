d0f751bddb93b6e34663d567f010cd60e97d264a
X_Smart <xchaigl@gmail.com>
2018-05-22 14:22:37 +0800

    Update GitStudy.java

--- src/cn/com/taiji/study/GitStudy.java
+++ src/cn/com/taiji/study/GitStudy.java
@@ -3,7 +3,7 @@ package cn.com.taiji.study;
 public class GitStudy {
 	public static void main(String[] args) {
 		System.out.println("hello GitHub");
-		System.out.println("hello GitHub");
+		System.out.println("你好 GitHub");
 	}
 
 }
