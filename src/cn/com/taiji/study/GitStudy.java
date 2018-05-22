471573e3c0ab91d908df47c3ac6465f145d889ad
xchaigl@gmail.com <xchaigl@gmail.com>
2018-05-22 14:23:40 +0800

    冲突测试

--- src/cn/com/taiji/study/GitStudy.java
+++ src/cn/com/taiji/study/GitStudy.java
@@ -3,7 +3,8 @@ package cn.com.taiji.study;
 public class GitStudy {
 	public static void main(String[] args) {
 		System.out.println("hello GitHub");
-		System.out.println("hello GitHub");
+		System.out.println("nihao a GitHub");
+		
 	}
 
 }
